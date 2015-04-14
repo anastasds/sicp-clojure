8a1f1e7ab1fa43e1d7b47db41626b2acbbaa4120
Anastas Stoyanovsky <contact@anastas.eu>
2015-04-14 16:00:50 -0400

    through exercise 1.33

--- ch1/section-1.3.clj
+++ ch1/section-1.3.clj
@@ -48,3 +48,115 @@
      dx))
 (integral cube 0 1 0.01) ; -> 0.24998750000000042
 (integral cube 0 1 0.001) ; -> 0.249999875000001
+
+;; Exercise 1.29
+(defn simpsons-rule [f a b n]
+  (def h (/ (- b a) n))
+  (defn add-dx [x] (+ x h))
+  (* (/ h 3)
+     (+ (f a)
+        (reduce + (map #(reduce * %) (map vector
+                                          (take (- n 1) (cycle [4 2]))
+                                          (map #(f (+ a (* % h))) (range 1 n)))))
+        (f b))))
+
+;; (defn simpsons-rule [f a b n]
+;;   (def h (/ (- b a) n))
+;;   (defn y [k] (f (+ a (* k h))))
+;;   (defn simpsons-term [k]
+;;     (* (cond (odd? k) 4
+;;              (or (= k 0) (= k n)) 1
+;;              (even? k) 2)
+;;        (y k)))
+;;   (/ (* h (sum simpsons-term 0 inc n)) 3))
+
+(simpsons-rule cube 0 1 100) ; -> 1/4
+
+
+;; Exercise 1.30
+(defn sum [term a next b]
+  (defn iter [a result]
+    (if (> a b)
+      result
+      (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+
+;; Exercise 1.31
+;; Part A
+(defn product [term a next b]
+  (if (> a b)
+    1
+    (* (term a)
+       (product term (next a) next b))))
+(defn factorial [n]
+  (product identity 1 inc n))
+(factorial 5) ; -> 120
+
+(defn pi-product [a b]
+  (defn pi-term [k]
+    (cond  
+      (= k 0) (/ 2 3)
+      (odd? k) (/ (+ k 3) (+ k 2))
+      (even? k) (/ (+ k 2) (+ k 3))))
+  (defn pi-next [x] (inc x))
+  (product pi-term a pi-next b))
+(double (* 4 (pi-product 0 1000))) ; -> 3.140026946105016
+
+;; Part B
+(defn product [term a next b]
+  (defn iter [a result]
+    (if (> a b)
+      result
+      (iter (next a) (* result (term a)))))
+  (iter a 1))
+(double (* 4 (pi-product 0 1000))) ; -> 3.140026946105016
+
+
+;; Exercise 1.32
+;; Part A
+(defn accumulate [combiner null-value term a next b]
+  (if (> a b)
+    null-value
+    (combiner (term a)
+              (accumulate combiner null-value term (next a) next b))))
+
+(defn sum [term a next b]
+  (accumulate + 0 term a next b))
+(sum identity 0 inc 10) ; -> 55
+
+(defn product [term a next b]
+  (accumulate * 1 term a next b))
+(product identity 1 inc 5) ; -> 120
+
+;; Part B
+(defn accumulate [combiner null-value term a next b]
+  (defn iter [a result]
+    (if (> a b)
+      result
+      (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+(sum identity 0 inc 10) ; -> 55
+(product identity 1 inc 5) ; -> 120
+
+
+;; Exercise 1.33
+(defn filtered-accumulate [combiner null-value term a next b predicate?]
+  (if (> a b)
+    null-value
+    (combiner
+     (if (predicate? a) (term a) null-value)
+     (filtered-accumulate combiner null-value term (next a) next b predicate?))))
+
+;; Part A
+(defn sum-squares-primes [a b]
+  (filtered-accumulate + 0 square a inc b prime?))
+(sum-squares-primes 3 8) ; -> 83
+
+;; Part B
+(defn sum-relatively-prime [n]
+  (defn relatively-prime? [k]
+    (= (gcd n k) 1))
+  (filtered-accumulate + 0 identity 2 inc (dec n) relatively-prime?))
+(sum-relatively-prime 10) ; -> 19
